3e82f34842e1e0c65f426746460bef9883be98a7
men21049 <men21049@byui.edu>
2023-01-05 00:28:52 -0600

    Ex. 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        int courseGrade = int.Parse(Console.ReadLine());
+        string letter;
+        string message = "Congratulations, you passed with a ";
+        string sign = "+";
+        int modGrade = courseGrade % 10;
+
+        if (modGrade < 7)
+        {
+            sign = "-";
+        }
+
+        if (courseGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (courseGrade < 90 && courseGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (courseGrade < 80 && courseGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (courseGrade < 70 && courseGrade >= 60)
+        {
+            letter = "D";
+            message = "You did not passed, your grade is ";
+        }
+        else
+        {
+            letter = "F";
+            message = "You did not passed, your grade is ";
+        }
+
+        if (courseGrade >= 97 || letter == "F")
+        {
+            Console.WriteLine($"{message} {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"{message} {letter}{sign}");
+        }
     }
 }
