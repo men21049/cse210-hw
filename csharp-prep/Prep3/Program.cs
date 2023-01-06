eae542ea448afced36467a1969f86870dfc0f897
men21049 <men21049@byui.edu>
2023-01-05 19:13:32 -0600

    Ex 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rd = new Random();
+        int randNum = rd.Next(1, 20);
+        int counter = 0;
+        int numGuess = 0;
+        bool wantToPlay = true;
+        string question = "What is your guess? ";
+        string keepPlaying = "yes";
+
+        while (wantToPlay)
+        {
+            Console.WriteLine($"{question}");
+            numGuess = int.Parse(Console.ReadLine());
+            counter += 1;
+            if (numGuess == randNum)
+            {
+                Console.WriteLine("You guessed it!, Do you want to keep playing?(yes/no) ");
+                keepPlaying = Console.ReadLine();
+                if (keepPlaying == "no")
+                {
+                    wantToPlay = false;
+                }
+            }
+            else if (numGuess < randNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }
     }
 }
