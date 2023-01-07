f3761962f82698cc3feb8a01327c82a5f416b168
men21049 <men21049@byui.edu>
2023-01-07 12:01:35 -0600

    ex 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+        string name = PromptUserName();
+        int squareNumber = SquareNumber(PromptUserNumber());
+        Console.WriteLine($"Brother {name}, the square of your number is {squareNumber}");
+
+
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favNumber = int.Parse(Console.ReadLine());
+        return favNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
     }
 }
