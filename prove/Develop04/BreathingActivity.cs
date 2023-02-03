b100bb2416bb5383ccd316e9ab292d5e909b69d8
men21049 <men21049@byui.edu>
2023-02-03 12:07:56 -0600

    dev

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -3,7 +3,7 @@ using System;
 class BreathingActivity : Activity
 {
 
-    public BreathingActivity() : base(
+    public BreathingActivity(string _activityName, string _description, List<string> activityList) : base(_activityName, _description, activityList)
     {
 
     }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,7 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Menu Options");
-        
+        string description = "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.";
+        List<string> activityList = new List<string> { "Breath in", "Breath out" };
+
+        BreathingActivity newAct = new BreathingActivity("breathing", description, activityList);
+
+
+        Activity.DisplayMenu();
+
+
     }
 }
